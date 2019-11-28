2ab25afc9e3223fad863dc0d1f710354806e044d
alexisgauvrit <alexis.gauvrit.85@gmail.com>
2019-11-28 14:04:08 +0100

    Init with express / gitignore / package.json

--- Dist/js/main.js
+++ Dist/js/main.js
@@ -1,3 +1,45 @@
+var socket = io.connect('http://localhost');
+
+socket.on('add_new', function(comment) {
+    //addTask(task);
+    console.log(comment);
+});
+
+
+/*$('#add').click(function () {
+  var task = new Object();
+  task.index = $('#todoList').children('.one_task').length;
+  task.text = $('#task').val();
+  socket.emit('add_task', task);
+
+  addTask(task);
+
+  $('.delete').unbind('click').click(function () {
+      removeTask($(this).data('index'));
+  });
+
+  $('#task').val("");
+});
+
+socket.on('add_task', function(task) {
+    addTask(task);
+
+    $('.delete').unbind('click').click(function () {
+      removeTask($(this).data('index'));
+    });
+});
+
+socket.on('get_todoList', function(todolist) {
+    $(todolist).each(function(){
+      addTask(this);
+    });
+});
+
+function addTask(task){
+  var html = '<li class="one_task task_' + task.index + '">' + task.text + '<a class="delete" data-index="' + task.index + '">✘</a></li>';
+  $('#todoList').append(html);
+}*/
+
 $(document).ready(function () {
 
     /* SET AND DISPLAY THE PREVIEW */
@@ -94,6 +136,11 @@ $(document).ready(function () {
             }
         });
         
+        return false;
+    });
+
+    $('#btnAddComment').on('click', function(){
+        socket.emit('add_new', "Ceci est un test !");
         return false;
     });
 });
--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-var http = require('http');
-var fs = require('fs');
-
-var server = http.createServer(function(req, res) {
-    fs.readFile('./Views/index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
-
+var express = require('express');
+var app = express();
+var server = app.listen(80);
+var ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
 var io = require('socket.io').listen(server);
 
 var commentList = [];
+
+app.use(express.static('Dist'));
+/* On affiche la todolist et le formulaire */
+app.get('/', function(req, res) { 
+    res.render('index.ejs');
+})
+/* On redirige vers la todolist si la page demandée n'est pas trouvée */
+.use(function(req, res, next){
+    res.redirect('/');
+});
+
 io.sockets.on('connection', function (socket) {
-    socket.emit('get_todoList', commentList);
+    socket.emit('get_commentList', commentList);
 
     socket.on('join', function (pseudo) {
-        socket.broadcast.emit('user_join', pseudo);
+        socket.broadcast.emit('user_join', ent.encode(pseudo));
     });
 
     socket.on('add_new', function (comment) {
-        todolist.push(comment);
-        socket.broadcast.emit('add_new', comment);
+        commentList.push(ent.encode(comment));
+        socket.broadcast.emit('add_new', ent.encode(comment));
     });
 });
-
-server.listen(8080);
